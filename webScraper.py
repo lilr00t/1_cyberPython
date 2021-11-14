ebb303f53a9386ccd60a93616b53e6169002dded
lilr00t <marc.l.sanchez.33@gmail.com>
2021-11-14 00:43:36 -0700

    added human read-ablility to show costs per class

--- webScraper.py
+++ webScraper.py
@@ -6,4 +6,7 @@ with open('testSite.html', 'r') as html_file:
     soup = BeautifulSoup(content, 'lxml')
     course_cards = soup.find_all('div', class_='card')
     for course in course_cards:
-        print(course.h5)
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        print(f'{course_name} costs {course_price}')
