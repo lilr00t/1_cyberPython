ac067dcd8a34607c839cd75bdda22dcb974ddf6b
lilr00t <marc.l.sanchez.33@gmail.com>
2021-11-15 22:16:11 -0700

    sanitized user input slightly to add upper input and upper required skills to ensure specifics for finding

--- webScraperTwo.py
+++ webScraperTwo.py
@@ -8,22 +8,21 @@ print(f'Filtering out {unfamiliar_skill}')
 
 
 def find_jobs():
-    html_text = requests.get(
-        'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=cyber&txtLocation=').text
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=cyber&txtLocation=').text
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    for job in jobs:
+    for index, job in enumerate(jobs):
         pub_date = job.find('span', class_='sim-posted').span.text
         if 'few' in pub_date:
             company_name = job.find('h3', class_='joblist-comp-name').text.replace('  ', '').upper()
             skills = job.find('span', class_='srp-skills').text.replace(' ', '').upper()
             more_info = job.header.h2.a['href']
             if unfamiliar_skill not in skills:
-                print(f"Company Name: {company_name.strip()}")
-                print(f"Required Skills: {skills.strip()}")
-                print(f"More Info: {more_info}")
-
-                print('')
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()}")
+                    f.write(f"Required Skills: {skills.strip()}")
+                    f.write(f"More Info: {more_info}")
+                print(f'File saved: {index}')
 
 
 if __name__ == '__main__':
